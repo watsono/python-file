85be10417fab76c4c4462c93e7d32ca5c1a48d94
watsono <314016491@qq.com>
2019-11-21 20:59:51 +0800

    Update 斗罗大陆下载.py
    11月21号修改时仍然可以使用

--- 斗罗大陆下载.py
+++ 斗罗大陆下载.py
@@ -1,14 +1,19 @@
+#爬取的网站为https://www.sangwu.org/
+#可以通过修改url来爬取特定的书籍
+#仅可自己使用，不可商业用途
+
+
 #!/usr/bin/env python3
 # -*- coding:utf-8 -*-
 import requests
 import re
-url = 'https://www.sangwu.org/book/0/308/'
+url = 'https://www.sangwu.org/book/0/308/' #修改url爬取数据
 response = requests.get(url)
-response.encoding = 'gbk'
+response.encoding = 'gbk' # 网站的格式
 html = response.text
 dl = re.findall(r'<dl>.*?</dl>',html,re.S)[0]
 title = re.findall(r'<dt>(.*?)</dt>',dl)[0]
-fb = open('%s.txt' % title, 'w', encoding='utf-8')
+fb = open('自己设置的路径/%s.txt' % title, 'w', encoding='utf-8') #会在此路径下下载斗罗大陆
 chapter_info_list = re.findall(r'href="(.*?)">(.*?)<',dl)
 for chapter_info in chapter_info_list:
     chapter_url = chapter_info[0]
@@ -29,4 +34,4 @@ for chapter_info in chapter_info_list:
     fb.write('\n')  
     fb.write('\n')    
     print(chapter_title)
-print(title) 
+print(title) #输出章节名称
