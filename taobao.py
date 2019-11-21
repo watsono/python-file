c01a22c5be48da47b22e894c2cfdaf9830aefefe
watsono <314016491@qq.com>
2019-11-21 20:27:24 +0800

    Update taobao.py

--- taobao.py
+++ taobao.py
@@ -13,22 +13,12 @@ url3 = '&ntoffset=4'
 
 def main():
     headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:68.0) Gecko/20100101 Firefox/68.0',
-               'Cookie':'cna=HAauFcFY2xoCASQHM5adoovj; t=74bd027388e6a20f9ca00aae6002c05c; '
-                        'isg=BNTUgu0_dcLa7OHsBrmykBlRphKGhfh-DCmROG61bN_iWXSjljghp-fbWZHkujBv; '
-                        'l=cBIlkfSHqa5avcKsBOCwquI8aO7TjIOYYuPRwNVXi_5dE6L_sLbOk7aSDFp6csWdTGLB450m8ze9-etkiKy06Pt-g3fP.; '
-                        'uc3=vt3=F8dBy3zdlK2cNikfUMI%3D&id2=VyyX4U0dFoSkYA%3D%3D&nk2=FPjAYXgwUOI%3D&lg2=VFC%2FuZ9ayeYq2g%3D%3D; tracknick=watsooon; '
-                        'lgc=watsooon; _cc_=U%2BGCWk%2F7og%3D%3D; tg=0; '
-                        'enc=UEMVhtlmnrUJpQ1OoqJfjOY72p8%2BW3Or3rJNyFjFc1wKgLNAzCtr411BFet3pasaUlZXbrdLs47mvNcfffSn2Q%3D%3D; miid=915240311857798560; '
-                        'thw=cn; UM_distinctid=16c033696d1102-011bdd2d22c227-4c312272-1fa400-16c033696d2345; '
-                        'x=e%3D1%26p%3D*%26s%3D0%26c%3D0%26f%3D0%26g%3D0%26t%3D0%26__ll%3D-1%26_ato%3D0; '
-                        'cookie2=7cccf262f9d938ec638cb27d82df0ca4; v=0; _tb_token_=e5f37574365e3; uc1=cookie14=UoTaHPCCA4tVfQ%3D%3D; '
-                        'mt=ci=-1_0; JSESSIONID=7E7BB91527A01257027752D0AC65B734; alitrackid=www.taobao.com; lastalitrackid=www.taobao.com; '
-                        'hng=CN%7Czh-CN%7CCNY%7C156; swfstore=113178'}
+               'Cookie':'cookies信息（浏览器中查找自己的）'}
 
     j = 1
     dic = {}
     while j <= page:
-        num = str((j - 1) * 44)
+        num = str((j - 1) * 44) #需要爬取的页数
         url = url1 + commodity + url2 + num + url3
         print(url)
         response = requests.get(url, headers=headers)
